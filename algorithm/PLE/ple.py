4421df5e34d30a02f438da5cad2b9329888870a0
tangxyw <tangxyw@hotmail.com>
2022-02-07 14:32:50 +0800

    modify ple.py

--- algorithm/PLE/ple.py
+++ algorithm/PLE/ple.py
@@ -13,6 +13,7 @@ import tensorflow as tf
 from tensorflow import feature_column as fc
 from extraction_network import extraction_network
 from MMOE.tower_layer import tower_layer
+from utils import train_input_fn, eval_input_fn
 
 
 
@@ -141,51 +142,6 @@ def example_parser(serialized_example):
     features = tf.parse_example(serialized_example, features=feature_spec)
     labels = {task_name: features.pop(task_name) for task_name in FLAGS.task_names.split(",")}
     return features, labels
-
-
-def train_input_fn(filepath, example_parser, batch_size, num_epochs, shuffle_buffer_size):
-    """
-        ple模型的input_fn
-    Args:
-        filepath (str): 训练集/验证集的路径
-        example_parser (function): 解析example的函数
-        batch_size (int): 每个batch样本大小
-        num_epochs (int): 训练轮数
-        shuffle_buffer_size (inr): shuffle时buffer的大小
-
-    Returns:
-        dataset
-    """
-
-    dataset = tf.data.TFRecordDataset(filepath)
-    if shuffle_buffer_size > 0:
-        dataset = dataset.shuffle(shuffle_buffer_size)
-    dataset = dataset.repeat(num_epochs)
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.map(example_parser, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.prefetch(1)
-
-    return dataset
-
-
-def eval_input_fn(filepath, example_parser, batch_size):
-    """
-        ple模型的eval阶段input_fn
-    Args:
-        filepath (str): 训练集/验证集的路径
-        example_parser (function): 解析example的函数
-        batch_size (int): 每个batch样本大小
-
-    Returns:
-        dataset
-    """
-
-    dataset = tf.data.TFRecordDataset(filepath)
-    dataset = dataset.batch(batch_size)
-    dataset = dataset.map(example_parser, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.prefetch(1)
-
-    return dataset
 
 
 def ple_model_fn(features, labels, mode, params):
