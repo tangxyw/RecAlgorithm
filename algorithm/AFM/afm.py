47ec78f7ed17c9fea6d5b845c4526be26bd3cb89
tangxyw <tangxyw@hotmail.com>
2022-02-18 16:32:16 +0800

    add AFM model

---
+++ algorithm/AFM/afm.py
@@ -0,0 +1,314 @@
+"""
+    [1] Xiao, Jun, et al. "Attentional factorization machines: Learning the weight of feature interactions via attention networks."
+    arXiv preprint arXiv:1708.04617 (2017).
+"""
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('..'))
+from typing import List, Tuple, Any
+import pandas as pd
+import tensorflow as tf
+from tensorflow import feature_column as fc
+from utils import train_input_fn, eval_input_fn
+
+
+# 定义输入参数
+flags = tf.app.flags
+
+# 训练参数
+flags.DEFINE_string("model_dir", "./model_dir", "Directory where model parameters, graph, etc are saved")
+flags.DEFINE_string("output_dir", "./output_dir", "Directory where pb file are saved")
+flags.DEFINE_string("train_data", "../../dataset/wechat_algo_data1/tfrecord/train.tfrecord", "Path to the train data")
+flags.DEFINE_string("eval_data", "../../dataset/wechat_algo_data1/tfrecord/test.tfrecord",
+                    "Path to the evaluation data")
+flags.DEFINE_string("vocabulary_dir", "../../dataset/wechat_algo_data1/vocabulary/",
+                    "Folder where the vocabulary file is stored")
+flags.DEFINE_integer("num_epochs", 1, "Epoch of training phase")
+flags.DEFINE_integer("train_steps", 10000, "Number of (global) training steps to perform")
+flags.DEFINE_integer("shuffle_buffer_size", 10000, "Dataset shuffle buffer size")
+flags.DEFINE_integer("num_parallel_readers", -1, "Number of parallel readers for training data")
+flags.DEFINE_integer("save_checkpoints_steps", 1000, "Save checkpoints every this many steps")
+
+# 模型参数
+flags.DEFINE_integer("batch_size", 1024, "Training batch size")
+flags.DEFINE_float("learning_rate", 0.005, "Learning rate")
+flags.DEFINE_integer("embedding_dim", 8, "Embedding dimension")
+flags.DEFINE_integer("attention_factor", 128, "Hidden layer size of the attention network")
+
+FLAGS = flags.FLAGS
+
+
+def create_feature_columns() -> Tuple[list, list, list]:
+    """
+
+    Returns:
+        dense_feature_columns (list): 连续特征的feature_columns
+        category_feature_columns (list): 类别特征的feature_columns
+        label_feature_columns (list): 因变量的feature_columns
+    """
+
+    category_feature_columns, dense_feature_columns = [], []
+    label_feature_columns = []
+
+    # 连续特征
+    videoplayseconds = fc.numeric_column('videoplayseconds', default_value=0.0)
+    u_read_comment_7d_sum = fc.numeric_column('u_read_comment_7d_sum', default_value=0.0)
+    u_like_7d_sum = fc.numeric_column('u_like_7d_sum', default_value=0.0)
+    u_click_avatar_7d_sum = fc.numeric_column('u_click_avatar_7d_sum', default_value=0.0)
+    u_forward_7d_sum = fc.numeric_column('u_forward_7d_sum', default_value=0.0)
+    u_comment_7d_sum = fc.numeric_column('u_comment_7d_sum', default_value=0.0)
+    u_follow_7d_sum = fc.numeric_column('u_follow_7d_sum', default_value=0.0)
+    u_favorite_7d_sum = fc.numeric_column('u_favorite_7d_sum', default_value=0.0)
+
+    i_read_comment_7d_sum = fc.numeric_column('i_read_comment_7d_sum', default_value=0.0)
+    i_like_7d_sum = fc.numeric_column('i_like_7d_sum', default_value=0.0)
+    i_click_avatar_7d_sum = fc.numeric_column('i_click_avatar_7d_sum', default_value=0.0)
+    i_forward_7d_sum = fc.numeric_column('i_forward_7d_sum', default_value=0.0)
+    i_comment_7d_sum = fc.numeric_column('i_comment_7d_sum', default_value=0.0)
+    i_follow_7d_sum = fc.numeric_column('i_follow_7d_sum', default_value=0.0)
+    i_favorite_7d_sum = fc.numeric_column('i_favorite_7d_sum', default_value=0.0)
+
+    c_user_author_read_comment_7d_sum = fc.numeric_column('c_user_author_read_comment_7d_sum', default_value=0.0)
+
+    dense_feature_columns += [videoplayseconds, u_read_comment_7d_sum, u_like_7d_sum, u_click_avatar_7d_sum,
+                              u_forward_7d_sum, u_comment_7d_sum, u_follow_7d_sum, u_favorite_7d_sum,
+                              i_read_comment_7d_sum, i_like_7d_sum, i_click_avatar_7d_sum, i_forward_7d_sum,
+                              i_comment_7d_sum, i_follow_7d_sum, i_favorite_7d_sum,
+                              c_user_author_read_comment_7d_sum]
+
+    # 类别特征
+    userid = fc.categorical_column_with_vocabulary_file('userid', os.path.join(FLAGS.vocabulary_dir, 'userid.txt'))
+    feedid = fc.categorical_column_with_vocabulary_file('feedid', os.path.join(FLAGS.vocabulary_dir, 'feedid.txt'))
+    device = fc.categorical_column_with_vocabulary_file('device', os.path.join(FLAGS.vocabulary_dir, 'device.txt'))
+    authorid = fc.categorical_column_with_vocabulary_file('authorid',
+                                                          os.path.join(FLAGS.vocabulary_dir, 'authorid.txt'))
+    bgm_song_id = fc.categorical_column_with_vocabulary_file('bgm_song_id',
+                                                             os.path.join(FLAGS.vocabulary_dir, 'bgm_song_id.txt'))
+    bgm_singer_id = fc.categorical_column_with_vocabulary_file('bgm_singer_id',
+                                                               os.path.join(FLAGS.vocabulary_dir, 'bgm_singer_id.txt'))
+    manual_tag_list = fc.categorical_column_with_vocabulary_file('manual_tag_list',
+                                                                 os.path.join(FLAGS.vocabulary_dir, 'manual_tag_id.txt'))
+
+    userid_emb = fc.embedding_column(userid, FLAGS.embedding_dim)
+    feedid_emb = fc.embedding_column(feedid, FLAGS.embedding_dim)
+    device_emb = fc.embedding_column(device, FLAGS.embedding_dim)
+    authorid_emb = fc.embedding_column(authorid, FLAGS.embedding_dim)
+    bgm_song_id_emb = fc.embedding_column(bgm_song_id, FLAGS.embedding_dim)
+    bgm_singer_id_emb = fc.embedding_column(bgm_singer_id, FLAGS.embedding_dim)
+    manual_tag_id_emb = fc.embedding_column(manual_tag_list, FLAGS.embedding_dim, combiner='mean')
+
+    category_feature_columns += [userid_emb, feedid_emb, device_emb, authorid_emb, bgm_song_id_emb,
+                                 bgm_singer_id_emb, manual_tag_id_emb]
+
+    # label
+    read_comment = fc.numeric_column("read_comment", default_value=0.0)
+    label_feature_columns += [read_comment]
+
+    return dense_feature_columns, category_feature_columns, label_feature_columns
+
+
+def example_parser(serialized_example):
+    """
+        批量解析Example
+    Args:
+        serialized_example:
+
+    Returns:
+        features, labels
+    """
+    fea_columns = total_feature_columns
+    label_columns = label_feature_columns
+
+    feature_spec = tf.feature_column.make_parse_example_spec(fea_columns + label_columns)
+    features = tf.parse_example(serialized_example, features=feature_spec)
+    read_comment = features.pop("read_comment")
+    return features, {"read_comment": read_comment}
+
+
+def afm_model_fn(features, labels, mode, params):
+    """
+        afm模型的model_fn
+    Args:
+        features (dict): input_fn的第一个返回值, 模型输入样本特征
+        labels (dict): input_fn的第二个返回值, 样本标签
+        mode: tf.estimator.ModeKeys
+        params (dict): 模型超参数
+
+    Returns:
+        tf.estimator.EstimatorSpec
+    """
+
+    # 连续特征
+    with tf.variable_scope("dense_input"):
+        dense_input = fc.input_layer(features, params["dense_feature_columns"])
+        dense_logit = tf.layers.dense(dense_input, 1, name="dense_logit")  # [batch, 1]
+
+    # 类别特征
+    with tf.variable_scope("category_input"):
+        category_input = fc.input_layer(features, params["category_feature_columns"])
+
+    with tf.variable_scope("pair_interaction_part"):
+        # 将每个类别特征的embedding取出
+        fields_embeddings = []
+        for cat_feature_column in params["category_feature_columns"]:
+            embed_input = fc.input_layer(features, [cat_feature_column])  # (batch, K)
+            fields_embeddings.append(embed_input)   # (batch, K) * F
+
+        F = len(params["category_feature_columns"])
+        # 存放两两field的哈达玛积
+        pair_hadamard_embeddings = []
+        for i in range(F):
+            for j in range(i+1, F):
+                pair_hadamard_embeddings.append(tf.multiply(fields_embeddings[i], fields_embeddings[j]))    # (batch, K) * F*(F-1)/2
+        pair_hadamard_embeddings = tf.stack(pair_hadamard_embeddings, axis=1)   # (batch, F*(F-1)/2, K)
+
+    with tf.variable_scope("attention_part"):
+        w = tf.get_variable(name="attention_w",
+                            shape=(params["embedding_dim"], params["attention_factor"]),
+                            dtype=tf.float32)   # (K, t)
+        b = tf.get_variable(name="attention_b",
+                            shape=(params["attention_factor"]),
+                            dtype=tf.float32)   # (t)
+        h = tf.get_variable(name="attention_h",
+                            shape=(params["attention_factor"], 1),
+                            dtype=tf.float32)  # (t, 1)
+
+        # attention_score = h^T * RELU(wX+b)
+        attention = tf.matmul(pair_hadamard_embeddings, w) + b  # (batch, F*(F-1)/2, K) matmul (K, t) + (t) = (batch, F*(F-1)/2, t)
+        attention = tf.nn.relu(attention)   # (batch, F*(F-1)/2, t)
+        attention = tf.matmul(attention, h)  # (batch, F*(F-1)/2, t) matmul (t, 1) = (batch, F*(F-1)/2, 1)
+        attention_score = tf.nn.softmax(attention, axis=1)  # (batch, F*(F-1)/2, 1)
+
+    with tf.variable_scope("prediction_score_part"):
+        weighted_sum = tf.multiply(pair_hadamard_embeddings, attention_score)   # (batch, F*(F-1)/2, K) * (batch, F*(F-1)/2, 1) = (batch, F*(F-1)/2, K)
+        weighted_sum = tf.reduce_sum(weighted_sum, axis=1)  # (batch, K)
+        p = tf.get_variable(name="p", shape=(params["embedding_dim"], 1), dtype=tf.float32)  # (K, 1)
+        afm_logit = tf.matmul(weighted_sum, p)  # (batch, K) matmul (K, 1) = (batch, 1)
+
+    total_logit = dense_logit + afm_logit
+
+    # -----定义PREDICT阶段行为-----
+    prediction = tf.sigmoid(total_logit, name="prediction")
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        predictions = {
+            "logit": total_logit,
+            'probabilities': prediction,
+        }
+        saved_model_output = {
+            'probabilities': prediction,
+        }
+        export_outputs = {
+            'prediction': tf.estimator.export.PredictOutput(saved_model_output)
+        }
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
+    # -----定义完毕-----
+
+    y = labels["read_comment"]
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=total_logit), name="loss")
+
+    accuracy = tf.metrics.accuracy(labels=y, predictions=tf.to_float(tf.greater_equal(prediction, 0.5)))
+    auc = tf.metrics.auc(labels=y, predictions=prediction)
+
+    # -----定义EVAL阶段行为-----
+    metrics = {"eval_accuracy": accuracy, "eval_auc": auc}
+    if mode == tf.estimator.ModeKeys.EVAL:
+        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
+    # -----定义完毕-----
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=params["learning_rate"], beta1=0.9,
+                                       beta2=0.999, epsilon=1e-8)
+    update_ops = tf.compat.v1.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+
+    # -----定义TRAIN阶段行为-----
+    assert mode == tf.estimator.ModeKeys.TRAIN
+
+    # tensorboard收集
+    tf.summary.scalar("train_accuracy", accuracy[1])
+    tf.summary.scalar("train_auc", auc[1])
+
+    # 训练log打印
+    log_hook = tf.train.LoggingTensorHook(
+        {
+            "train_loss": loss,
+            "train_auc": auc[1],
+            "dense_logit": dense_logit,
+            "afm_logit": afm_logit,
+            "attention_score": attention_score
+        },
+        every_n_iter=100
+    )
+
+    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op, training_hooks=[log_hook])
+    # -----定义完毕-----
+
+
+def main(unused_argv):
+    """训练入口"""
+
+    global total_feature_columns, label_feature_columns
+    dense_feature_columns, category_feature_columns, label_feature_columns = create_feature_columns()
+    total_feature_columns = dense_feature_columns + category_feature_columns
+
+    params = {
+        "category_feature_columns": category_feature_columns,
+        "dense_feature_columns": dense_feature_columns,
+        "embedding_dim": FLAGS.embedding_dim,
+        "attention_factor": FLAGS.attention_factor,
+        "learning_rate": FLAGS.learning_rate,
+    }
+    print(params)
+
+    estimator = tf.estimator.Estimator(
+        model_fn=afm_model_fn,
+        params=params,
+        config=tf.estimator.RunConfig(model_dir=FLAGS.model_dir, save_checkpoints_steps=FLAGS.save_checkpoints_steps)
+    )
+
+    train_spec = tf.estimator.TrainSpec(
+        input_fn=lambda: train_input_fn(filepath=FLAGS.train_data, example_parser=example_parser,
+                                        batch_size=FLAGS.batch_size, num_epochs=FLAGS.num_epochs,
+                                        shuffle_buffer_size=FLAGS.shuffle_buffer_size),
+        max_steps=FLAGS.train_steps
+    )
+
+    feature_spec = tf.feature_column.make_parse_example_spec(total_feature_columns)
+    serving_input_receiver_fn = tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_spec)
+    exporters = [
+        tf.estimator.BestExporter(
+            name="best_exporter",
+            serving_input_receiver_fn=serving_input_receiver_fn,
+            exports_to_keep=5)
+    ]
+    eval_spec = tf.estimator.EvalSpec(
+        input_fn=lambda: eval_input_fn(filepath=FLAGS.eval_data, example_parser=example_parser,
+                                       batch_size=FLAGS.batch_size),
+        throttle_secs=600,
+        steps=None,
+        exporters=exporters
+    )
+
+    tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
+
+    # Evaluate Metrics.
+    metrics = estimator.evaluate(input_fn=lambda: eval_input_fn(filepath=FLAGS.eval_data, example_parser=example_parser,
+                                                                batch_size=FLAGS.batch_size))
+    for key in sorted(metrics):
+        print('%s: %s' % (key, metrics[key]))
+
+    results = estimator.predict(input_fn=lambda: eval_input_fn(filepath=FLAGS.eval_data, example_parser=example_parser,
+                                                               batch_size=FLAGS.batch_size))
+    predicts_df = pd.DataFrame.from_dict(results)
+    predicts_df['probabilities'] = predicts_df['probabilities'].apply(lambda x: x[0])
+    test_df = pd.read_csv("../../dataset/wechat_algo_data1/dataframe/test.csv")
+    predicts_df['read_comment'] = test_df['read_comment']
+    predicts_df.to_csv("predictions.csv")
+    print("after evaluate")
+
+
+if __name__ == "__main__":
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run(main=main)
