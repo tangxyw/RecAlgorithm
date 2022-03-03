ef975c448124740dff14455de4d625cb573f684a
tangxyw <tangxyw@hotmail.com>
2022-03-03 22:32:24 +0800

    add BST result

---
+++ algorithm/FiBiNET/bilinear_interaction_layer.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import itertools
+
+
+def bilinear_interaction_layer(input, embedding_dim, type, name):
+    """
+        FiBinet模型中的Bilinear Interaction Layer实现
+    Args:
+        input (Tensor): 输入, shape=(batch, F, K)
+        embedding_dim (int): embedding维度K
+        type (str): Bilinear interaction类型, "all", "each", 或者 "interaction"
+        name (str): 用来区分不同的Bilinear Interaction Layer
+
+    Returns:
+        tf.Tensor, shape=(batch, F*(F-1)/2, K)
+    """
+
+    # 维度信息
+    F = input.get_shape(input)[1]
+
+    if type == "all":
+        w = tf.get_variable(name=f"{name}_w_all", shape=(embedding_dim, embedding_dim), dtype=tf.float32)   # (K, K)
+        v_w = tf.matmul(input, w)   # (batch, F, K) @ (K, K) = (batch, F, K)
+        p = [tf.multiply(v_w[:, i, :], input[:, j, :]) for i, j in itertools.combinations(range(F-1), 2)]   # (batch, K) * F*(F-1)/2
+
+    elif type == "each":
+        w = tf.get_variable(name=f"{name}_w_each", shape=(F-1, embedding_dim, embedding_dim), dtype=tf.float32)  # (F-1, K, K)
+        v_w = [tf.matmul(input[:, i, :], w[i, :, :]) for i in range(F-1)]   # (batch, K) * F-1
+        p = [tf.multiply(v_w[i], input[:, j, :]) for i, j in itertools.combinations(range(F-1), 2)] # (batch, K) * F*(F-1)/2
+
+    elif type == "interaction":
+        w = tf.get_variable(name=f"{name}_w_interaction", shape=(F*(F-1)//2, embedding_dim, embedding_dim), dtype=tf.float32)    # (F(F-1)/2, K, K)
+        p = [tf.multiply(tf.matmul(input[:, i, :], w[k, :, :]), input[:, j, :])
+             for (i, j), k in zip(itertools.combinations(range(F-1), 2), range(F*(F-1)//2))]   # (batch, K) * F*(F-1)/2
+
+    else:
+        raise ValueError(
+                f"Bilinear Interaction type must be in ['all','each','interaction'], got '{type}'")
+
+    p = tf.stack(p, axis=1)  # (batch, F*F(F-1)/2, K)
+
+    return p
---
+++ algorithm/FiBiNET/senet.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+
+def senet(input, embedding_dim, reduction_ratio):
+    """
+        SENET实现
+    Args:
+        input (Tensor): 输入, shape=(batch, F, K)
+        embedding_dim (int): embedding维度K
+        reduction_ratio (int): 缩减比率
+
+    Returns:
+        tf.Tensor, shape=(batch, F, K)
+    """
+
+    # 维度信息
+    F = input.get_shape()[1]
+    reduction_dim = embedding_dim // reduction_ratio
+
+    # 定义模型参数
+    w1 = tf.get_variable(name="senet_w1", shape=(F, reduction_dim), dtype=tf.float32)
+    w2 = tf.get_variable(name="senet_w2", shape=(reduction_dim, F), dtype=tf.float32)
+
+    # 计算权重向量a
+    z = tf.reduce_mean(input, axis=-1)  # (batch, F)
+    a = tf.matmul(z, w1)    # (batch, reduction_dim)
+    a = tf.nn.relu(a)   # (batch, reduction_dim)
+    a = tf.matmul(a, w2)  # (batch, F)
+    a = tf.nn.relu(a)  # (batch, F)
+    a = tf.expand_dims(a, axis=-1)  # (batch, F, 1)
+
+    # reweight
+    v = input * a   # (batch, F, K) * (batch, F, 1) = (batch, F, K)
+
+    return v
